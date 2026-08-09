ab91854d63cd39cd2583813cc6598c4a01e375dc
M.G <1617550506@qq.com>
2026-08-09 11:24:31 +0800

    修改了leetcode11的类型定义错误，使用python内置的list声明

--- 双指针/leetcode11.py
+++ 双指针/leetcode11.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
+    def maxArea(self, height: list[int]) -> int:
         """思路：双指针，由于隔板往中间靠拢，高的移动不如矮的移动，所以选择矮的夹板向内侧移动
            时间复杂度为O(n)，空间复杂度为O(1)"""
         n = len(height)
