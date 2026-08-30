dda20530cd07b5f22f5d8bdaaefeef18e5923fb5
M.G <1617550506@qq.com>
2026-08-30 11:41:41 +0800

    双指针和数学的融合

--- twopointer/leetcode160.cpp
+++ twopointer/leetcode160.cpp
@@ -10,6 +10,7 @@ struct ListNode {
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        //利用数学a+m+b=b+m+a和双指针的操作方式来查找交点，时间复杂度O(m+n)
         if(!headA) return NULL;
         if(!headB) return NULL;
         ListNode* nodeA=headA, *nodeB=headB;
