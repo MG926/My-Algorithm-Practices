ed83a7111e62f637ae20d9868b30318348f3781a
M.G <1617550506@qq.com>
2026-09-08 11:08:13 +0800

    利用异或的性质，难度：**

---
+++ math/leetcode136.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int k=0;
+        for(int i=0;i<n;i++){
+            k ^= nums[i];
+        }
+        return k;
+    }
+};
